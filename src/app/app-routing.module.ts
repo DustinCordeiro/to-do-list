bb2e4a5bdca2fe85a816c4aa9c1f184e98c5bf33
Dustin Leandro <dustin.cordeiro88@gmail.com>
2023-10-29 13:42:34 -0300

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './modules/home/home.module';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  HomeModule
+],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
